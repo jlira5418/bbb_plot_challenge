51cc6047be8547aa902d7c6968f8ccd237abf2ba
jlira5418 <jlira5418@gmail.com>
2022-01-07 00:20:06 -0600

    bubble

--- static/js/app.js
+++ static/js/app.js
@@ -2,20 +2,63 @@ const url = "samples.json";
 const dataPromise =d3.json(url);
 //console.log(dataPromise);
 
-d3.json(url).then(function(data) 
-{
-    var dropdown = document.getElementById("selDataset");
-    var options = data.names;
-
-    for(var i = 0; i < options.length; i++) 
+function initialRun(){
+    d3.json(url).then(function(data) 
     {
-        var optValue = options[i];
+        var dropdownSelect = document.getElementById("selDataset");
+        var listOfNames = data.names;
         var optElem = document.createElement("option");
-        //<option></option>
-        optElem.textContent = optValue;
-        //<option>Banana</option>
-        optElem.value = optValue;
-        //<option value="Banana">Banana</option>
-        dropdown.appendChild(optElem);
-    }
-});
+        // optElem.textContent = " ";
+        //optElem.value = " ";
+        //dropdownSelect.appendChild(optElem);
+
+        for(var i = 0; i < listOfNames.length; i++) 
+        {
+            var name = listOfNames[i];
+            var optElem = document.createElement("option");
+            //<option>name</option>
+            optElem.textContent = name;
+            //<option>Banana</option>
+            optElem.value = name;
+            //<option value="Banana">Banana</option>
+            dropdownSelect.appendChild(optElem);
+        }
+
+        //Select first item
+        var firstItem = listOfNames[0];
+        buildBubbleChart(firstItem);
+        //buildMetaData(firstItem);
+
+
+
+    });
+}
+
+function buildBubbleChart(itemid){
+   // alert(itemid);
+   d3.json(url).then(function(data)
+
+   { 
+        var samples  = data.samples;
+        //alert(samples);
+        var sampleFilter = samples.filter(sampleObject => sampleObject.id == itemid);
+        //alert(sampleFilter);
+        var result = sampleFilter[0];
+        var OTUids = result.otu_ids;
+        var OTUlbls = result.otu_labels;
+        var OTUsample = result.sample_values;
+        //alert(OTUids);
+        //alert(OTUlbls);
+        //alert(OTUsample);
+        var layoutBubble = {title:"Belly Button Biodiversity", xaxis:{title:"OTU ID"}, };
+        var dataBubble = [{x:OTUids, y:OTUsample, text: OTUlbls, mode:"markers", marker:{size:OTUsample, color:OTUids, colorscale:"Earth" }}];
+        Plotly.newPlot("bubble", dataBubble, layoutBubble);
+   });
+}
+
+function optionChanged(item){
+    buildBubbleChart(item);   
+}
+
+
+initialRun();
