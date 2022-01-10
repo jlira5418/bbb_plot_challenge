bdc6321df16b3e8a5fdf1c63fbe6c3c56b0e2029
jlira5418 <jlira5418@gmail.com>
2022-01-09 23:48:13 -0600

    metadata

--- static/js/app.js
+++ static/js/app.js
@@ -1,64 +1,121 @@
 const url = "samples.json";
-const dataPromise =d3.json(url);
-//console.log(dataPromise);
+const dataPromise = d3.json(url);
+var all_data;
 
-function initialRun(){
-    d3.json(url).then(function(data) 
-    {
-        var dropdownSelect = document.getElementById("selDataset");
-        var listOfNames = data.names;
+
+
+dataPromise.then(function (data) {
+    all_data = data;
+    var dropdownSelect = document.getElementById("selDataset");
+    var listOfNames = data.names;
+    
+    
+    for (var i = 0; i < listOfNames.length; i++) {
+        var name = listOfNames[i];
         var optElem = document.createElement("option");
-        // optElem.textContent = " ";
-        //optElem.value = " ";
-        //dropdownSelect.appendChild(optElem);
+     
+        optElem.textContent = name;
+        
+        optElem.value = name;
+       
+        dropdownSelect.appendChild(optElem);
+    }
 
-        for(var i = 0; i < listOfNames.length; i++) 
+    //Select first item
+    var firstItem = listOfNames[0];
+    buildBarChart(firstItem, data);
+    buildBubbleChart(firstItem, data);
+    buildMetaData(firstItem);
+
+    console.log(all_data);
+
+
+});
+
+function buildBarChart(itemid, data) {
+    
+
+    if (data === undefined)
+        data = all_data;
+
+
+    var samples = data.samples;
+    
+    var sampleFilter = samples.filter(sampleObject => sampleObject.id == itemid);
+    
+    var result = sampleFilter[0];
+    var dataBar = [
         {
-            var name = listOfNames[i];
-            var optElem = document.createElement("option");
-            //<option>name</option>
-            optElem.textContent = name;
-            //<option>Banana</option>
-            optElem.value = name;
-            //<option value="Banana">Banana</option>
-            dropdownSelect.appendChild(optElem);
+          x: result.sample_values.slice(0, 10).reverse(),
+          y: result.otu_ids.slice(0, 10).map(val=>"OTU " + val).reverse(),
+          type: 'bar',
+          orientation: "h",
+          text: result.otu_labels.slice(0, 10).reverse()
+
         }
-
-        //Select first item
-        var firstItem = listOfNames[0];
-        buildBubbleChart(firstItem);
-        //buildMetaData(firstItem);
-
-
-
-    });
+      ];
+      
+    Plotly.newPlot("bar", dataBar);
+    
+   
 }
 
-function buildBubbleChart(itemid){
-   // alert(itemid);
-   d3.json(url).then(function(data)
+function buildBubbleChart(itemid, data) {
+    
 
-   { 
-        var samples  = data.samples;
-        //alert(samples);
-        var sampleFilter = samples.filter(sampleObject => sampleObject.id == itemid);
-        //alert(sampleFilter);
-        var result = sampleFilter[0];
-        var OTUids = result.otu_ids;
-        var OTUlbls = result.otu_labels;
-        var OTUsample = result.sample_values;
-        //alert(OTUids);
-        //alert(OTUlbls);
-        //alert(OTUsample);
-        var layoutBubble = {title:"Belly Button Biodiversity", xaxis:{title:"OTU ID"}, };
-        var dataBubble = [{x:OTUids, y:OTUsample, text: OTUlbls, mode:"markers", marker:{size:OTUsample, color:OTUids, colorscale:"Earth" }}];
-        Plotly.newPlot("bubble", dataBubble, layoutBubble);
-   });
+    if (data === undefined)
+        data = all_data;
+
+
+    var samples = data.samples;
+    
+    var sampleFilter = samples.filter(sampleObject => sampleObject.id == itemid);
+    
+    var result = sampleFilter[0];
+    var OTUids = result.otu_ids;
+    var OTUlbls = result.otu_labels;
+    var OTUsample = result.sample_values;
+   
+    var layoutBubble = { title: "Belly Button Biodiversity", xaxis: { title: "OTU ID" }, };
+    var dataBubble = [{ x: OTUids, y: OTUsample, text: OTUlbls, mode: "markers", marker: { size: OTUsample, color: OTUids, colorscale: "Earth" } }];
+    Plotly.newPlot("bubble", dataBubble, layoutBubble);
+
+
+
 }
 
-function optionChanged(item){
-    buildBubbleChart(item);   
+function buildMetaData(itemid, data) {
+    
+
+    if (data === undefined)
+        data = all_data;
+
+
+    var samples = data.metadata;
+    
+    var sampleFilter = samples.filter(sampleObject => sampleObject.id == itemid);
+    var result = sampleFilter[0];
+    var metadatapanel = d3.select("#sample-metadata");
+    metadatapanel.html("");
+    metadatapanel.append("h6").text("id: " +itemid);
+    metadatapanel.append("h6").text("ethnicity: " +result.ethnicity);
+    metadatapanel.append("h6").text("gender: " +result.gender);
+    metadatapanel.append("h6").text("age: " +result.age);
+    metadatapanel.append("h6").text("location: "  +result.location);
+    metadatapanel.append("h6").text("bbtype: " +result.bbtype);
+    metadatapanel.append("h6").text("wfreq: " +result.wfreq);
+    //console.log(result);
+
+    
+
+}
+
+function optionChanged(item) {
+    buildBarChart(item);
+    buildBubbleChart(item);
+    buildMetaData(item);
 }
 
 
-initialRun();
+
+
